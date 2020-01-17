955a1c0b84efa6cc7fc0541eacd1e1eacd8e870e
Kerri Sierra-Scott <kerriss66@hotmail.com>
2020-01-16 19:36:07 -0600

    added controller, data, and models

--- src/CheeseMVC/Controllers/CheeseController.cs
+++ src/CheeseMVC/Controllers/CheeseController.cs
@@ -1,49 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using CheeseMVC.Models;
+using System.Collections.Generic;
 using CheeseMVC.ViewModels;
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+using CheeseMVC.Data;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace CheeseMVC.Controllers
 {
     public class CheeseController : Controller
     {
-
+        //private readonly CheeseDbContext context;
+        private CheeseDbContext context;
+        public CheeseController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            CheeseListViewModel model = new CheeseListViewModel {
-                Cheeses = CheeseData.GetAll()
-            };
+            IList<Cheese> cheeses = context.Cheeses.Include(c => c.Category).ToList();
 
-            return View(model);
+            return View(cheeses);
         }
 
         public IActionResult Add()
         {
+            AddCheeseViewModel addCheeseViewModel = new AddCheeseViewModel(context.Categories.ToList());
+            return View(addCheeseViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCheeseViewModel addCheeseViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                CheeseCategory newCheeseCategory = context.Categories.Single(c => c.ID == addCheeseViewModel.CategoryID);
+                // Add the new cheese to my existing cheeses
+                Cheese newCheese = new Cheese
+                {
+                    Name = addCheeseViewModel.Name,
+                    Description = addCheeseViewModel.Description,
+                    Category = newCheeseCategory
+                };
+
+                context.Cheeses.Add(newCheese);
+                context.SaveChanges();
+
+                return Redirect("/Cheese");
+            }
+
+            return View(addCheeseViewModel);
+        }
+
+        public IActionResult Remove()
+        {
+            ViewBag.title = "Remove Cheeses";
+            ViewBag.cheeses = context.Cheeses.ToList();
             return View();
         }
 
         [HttpPost]
-        [Route("/Cheese/Add")]
-        public IActionResult NewCheese(string name, string description)
+        public IActionResult Remove(int[] cheeseIds)
         {
-            // Add the new cheese to my existing cheeses
-            CheeseData.Add(new Cheese(name, description));
+            foreach (int cheeseId in cheeseIds)
+            {
+                Cheese theCheese = context.Cheeses.Single(x => x.ID == cheeseId);
+                //remove function needs object to remove so we have to create new cheese object the if cheeseid is equal to ID remove that object.
+                context.Cheeses.Remove(theCheese);
+            }
+            context.SaveChanges();
 
             return Redirect("/");
         }
-
-        public IActionResult Detail(int id)
-        {
-            ViewBag.cheese = CheeseData.GetById(id);
-            ViewBag.title = "Cheese Detail";
-            return View();
-        }
-
     }
 }
--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -1,41 +1,15 @@
-﻿namespace CheeseMVC.Models
+﻿using System.Collections.Generic;
+
+namespace CheeseMVC.Models
 {
     public class Cheese
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        
-        public int CheeseId { get; set; }
-        private static int nextId = 1;
 
-        public Cheese(string name, string description) : this()
-        {
-            Name = name;
-            Description = description;
-        }
-
-        public Cheese()
-        {
-            CheeseId = nextId;
-            nextId++;
-        }
-
-        // override object.Equals
-        public override bool Equals(object obj)
-        {
-
-            if (obj == null || GetType() != obj.GetType())
-            {
-                return false;
-            }
-            
-            return CheeseId == (obj as Cheese).CheeseId;
-        }
-
-        // override object.GetHashCode
-        public override int GetHashCode()
-        {
-            return CheeseId;
-        }
+        public CheeseCategory Category { get; set; }
+        public int ID { get; set; }
+        public int CategoryID { get; set; }
+        public IList<CheeseMenu> CheeseMenus { get; set; }
     }
 }
